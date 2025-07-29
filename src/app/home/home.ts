757c63c4e282ad06b31e20d8e9ec6ff1bd04267d
Nahary Toavina <toavina.rabenjanaharisoa@gmail.com>
2025-07-29 13:40:28 +0300

    call component on component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from '../housing-location/housing-location';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HousingLocation],
   styleUrl: './home.css',
   template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-  `,
+      <section>
+        <form>
+          <input type="text" placeholder="Filter by city" />
+          <button class="primary" type="button">Search</button>
+        </form>
+      </section>
+      <section class="results">
+        <app-housing-location></app-housing-location>
+      </section>
+    `,
 })
 export class Home {
 
